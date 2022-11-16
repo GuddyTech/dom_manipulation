673e195c921212ae94a28120c933f40c548d14b4
Okorie GoodGod <guddytechs@gmail.com>
2022-11-16 11:08:36 +0100

    appending elements

--- dom_manipulation/scripts/home.js
+++ dom_manipulation/scripts/home.js
@@ -4,6 +4,26 @@ console.log(bodyBox);
 bodyBox.style.backgroundColor = "red";
 
 // Removing Elements from the DOM
-
-const para = document.body.childNodes;
+const para = document.body.childNodes[3];
 console.log(para);
+
+const smallPara = document.getElementById("small_p");
+smallPara.style.color = "white";
+
+const bigText = document.querySelector("#head-text");
+// bigText.style.display = "none";
+
+// bigText.remove(); // to remove the h1 tag
+
+// removePara.removeChild(spanEls[0]); grabing a child by class name with index
+
+
+//appending elements to empty divs: the bigText goes down to where it was appended.
+const divEl = document.querySelector("div");
+console.log(divEl);
+divEl.append(bigText);
+
+
+
+
+
