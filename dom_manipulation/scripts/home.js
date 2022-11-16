bd866c757e95cb585d0eb3be6bf75fda233ec6ad
Okorie GoodGod <guddytechs@gmail.com>
2022-11-16 10:31:45 +0100

    adding a button

--- dom_manipulation/scripts/home.js
+++ dom_manipulation/scripts/home.js
@@ -2,3 +2,8 @@ const bodyBox = document.body;
 console.log(bodyBox);
 
 bodyBox.style.backgroundColor = "red";
+
+// Removing Elements from the DOM
+
+const para = document.body.childNodes;
+console.log(para);
